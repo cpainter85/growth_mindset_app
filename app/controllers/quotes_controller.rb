3b0c7f94398ed402d8b527d668db6f45c8479a58
Charles Painter <cpainter85@gmail.com>
2015-02-05 21:31:17 -0700

    Add new and create resources

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,25 @@ class QuotesController < ApplicationController
   def index
     @quotes = Quote.all
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:author, :content)
+  end
+
 end
